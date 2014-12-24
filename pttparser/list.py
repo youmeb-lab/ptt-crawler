44cd693f5dd0cde230281b97c549af0aee20f6d2
Jamie Yang <jamieyang@Jamies-MacBook-Pro-3.local>
2014-12-24 17:29:43 +0800

    Add html list parser

---
+++ pttparser/list.py
@@ -0,0 +1,12 @@
+
+
+from bs4 import BeautifulSoup
+
+__all__ = ("parse")
+
+
+def parse(html):
+    soup = BeautifulSoup(html)
+    links = soup.select(".r-ent a")
+    links = [link.get('href') for link in links]
+    return links
