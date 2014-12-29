cb29f7fd1757280fceb059ff015c43f7eb15119b
Po-Ying Chen <poying.me@gmail.com>
2014-12-29 17:24:07 +0800

    update list parser

--- pttparser/list.py
+++ pttparser/list.py
@@ -5,6 +5,30 @@ __all__ = ("parse")
 
 def parse(html):
     soup = BeautifulSoup(html)
+    url = parse_prev_page_url(soup)
+
+    if url is None:
+        return None
+
+    return {
+        "prev_page_url": url,
+        "article_list": parse_article_list(soup),
+    }
+
+
+def parse_prev_page_url(soup):
+    links = soup.select(".action-bar .pull-right .btn")
+    url = None
+
+    try:
+        url = links[1].get("href")
+    except:
+        pass
+
+    return url
+
+
+def parse_article_list(soup):
     links = soup.select(".r-ent a")
     links = [link.get('href') for link in links]
     return links
